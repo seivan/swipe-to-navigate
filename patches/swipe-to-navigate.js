86e71234b6ffa912772e406cb84a7180bacbc1a3
Seivan Heidari <seivan.heidari@icloud.com>
2019-10-19 18:56:11 +0200

    No longer override App

--- patches/swipe-to-navigate.js
+++ patches/swipe-to-navigate.js
@@ -1,20 +1,8 @@
-// define([
-//     "module",
-//     "require",
-//     "vs/platform/instantiation/common/instantiationService",
-//     "vs/code/electron-main/app",
-//     "vs/code/electron-main/window",
-//     "vs/base/common/platform",
-//     "vs/platform/configuration/common/configuration",
-//     "swipe-to-navigate/utils"
-// ], function (module, require, insantiationService, app, win, platform, configuration, utils) {
-
 define([
-    "vs/code/electron-main/app",
     "vs/code/electron-main/window",
     "vs/base/common/platform",
     "swipe-to-navigate/utils"
-], function (app, win, platform, utils) {
+], function (win, platform, utils) {
     'use strict';
 
 
@@ -60,45 +48,42 @@ define([
 
 
 
-    // 'description': nls.localize('swipeToNavigate', "Control what a three finger swipe will navigate between."),
+
 
     let isMacOS = platform.isMacintosh;
 
 
-    utils.override(app.CodeApplication, "openFirstWindow", function (openFirstWindow) {
-        let resultOpenFirstWindow = openFirstWindow();
+    utils.override(win.CodeWindow, "onConfigurationUpdated", function (onConfigurationUpdated) {
 
-        utils.override(win.CodeWindow, "onConfigurationUpdated", function (onConfigurationUpdated) {
-            let resultOnConfigurationUpdated = onConfigurationUpdated();
+        let resultOnConfigurationUpdated = onConfigurationUpdated();
 
-            let window = this;
+        let window = this;
 
-            let config = window.configurationService.getValue("swipeToNavigate");
+        let config = window.configurationService.getValue("swipeToNavigate");
 
-            let command = { event: null, back: null, forward: null };
+        let command = { event: null, back: null, forward: null };
 
-            if (isMacOS) {
-                command.event = "swipe";
-                command.back = "left";
-                command.forward = "right";
-            }
-            else {
-                command.event = "app-command";
-                command.back = "browser-backward";
-                command.forward = "browser-forward";
-            }
+        if (isMacOS) {
+            command.event = "swipe";
+            command.back = "left";
+            command.forward = "right";
+        }
+        else {
+            command.event = "app-command";
+            command.back = "browser-backward";
+            command.forward = "browser-forward";
+        }
 
-            window._win.removeAllListeners(command.event);
+        window._win.removeAllListeners(command.event);
 
-            if (config == null) { config = defaultConfig; }
-            if (config.action !== 'disabled') { registerSwipe(command, config, window); }
+        if (config == null) { config = defaultConfig; }
+        if (config.action !== 'disabled') { registerSwipe(command, config, window); }
 
 
-            return resultOnConfigurationUpdated;
-        });
-
-        return resultOpenFirstWindow;
-
+        return resultOnConfigurationUpdated;
     });
 
+    return resultOpenFirstWindow;
+
 });
+
